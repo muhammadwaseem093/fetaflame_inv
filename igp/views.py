b304076479303d561d91dc71e1971a1174cd0a0a
Muhammad Waseem <muhammadwaseem093@gmail.com>
2025-02-18 10:13:46 +0500

    modification in igp update funcionality

--- igp/views.py
+++ igp/views.py
@@ -178,27 +178,54 @@ def update_igp_items(request, igp_number):
     units = Unit.objects.all()
 
     if request.method == 'POST':
-        num_items = len(request.POST) // 4  # Calculate number of items submitted based on the number of fields
+        num_items = len(request.POST) // 5  # Calculate number of items submitted based on the number of fields
 
         for i in range(1, num_items + 1):
             item_key = f'item_{i}'
+            item_no_key=f'item_no_{i}'
             description_key = f'description_{i}'
             unit_key = f'unit_{i}'
             quantity_key = f'quantity_{i}'
 
             item_id = request.POST.get(item_key)
+            item_no=request.POST.get(item_no_key)
             description = request.POST.get(description_key, '')
             unit = request.POST.get(unit_key)
             quantity = request.POST.get(quantity_key)
-
+            
             if item_id:
-                igp_items = IGPItem.objects.filter(igp=igp, item_id=item_id)
-                if igp_items.exists():
-                    for igp_item in igp_items:
-                        igp_item.description = description
-                        igp_item.unit_id = unit
-                        igp_item.quantity = quantity
-                        igp_item.save()
+                item= Item.objects.get(id=item_id)
+                igp_item = IGPItem(
+                    igp=igp,
+                    item=item,
+                    item_no=item.item_no,
+                    description=description,
+                    unit=unit,
+                    quantity=quantity
+                )
+                
+                igp_item.save()
+            
+            # item = Item.objects.get(id=item_id)
+            # if item_id:
+            #     data = {
+            #         'item': item.id,
+            #         'item_no':item.item_no,
+            #         'description': description,
+            #         'unit': unit,
+            #         'quantity': quantity
+            #     }
+
+            #     form = IGPItemForm(data)
+            #     if form.is_valid():
+            #         igp_item = form.save(commit=False)
+            #         igp_item.igp = igp
+            #         igp_item.save()
+            #     else:
+            #         for field, error in form.errors.items():
+            #             print(f"Field: {field} - Errors: {error}")
+            #         item_forms.append(form)
+            #         return JsonResponse({"success": False, "message": f"Invalid data for item {i}. Errors: {form.errors}"}, status=400)
 
         return redirect('igp_list')  # Redirect to the list page after successful update
 
